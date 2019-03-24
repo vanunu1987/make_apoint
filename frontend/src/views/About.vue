1a6452985ba888ce41a6815ce8db84eec3f9dba5
Your Name <vanunu24@gmail.com>
2019-03-24 19:21:08 +0200

    qwrwr

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -4,6 +4,15 @@
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum incidunt est, officiis accusamus voluptates expedita nam fuga error, inventore harum dolores alias autem exercitationem fugiat! Ea rerum tenetur pariatur obcaecati?
     </p>
-
+     <Work-hours/>
   </div>
 </template>
+
+<script>
+import WorkHours from '@/components/WorkHours.vue'
+export default {
+  components:{
+    WorkHours
+  }
+}
+</script>
