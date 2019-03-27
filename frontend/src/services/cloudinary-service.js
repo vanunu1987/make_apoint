b8d661e3c27c0754b0eb3eed44d03e647027315f
Your Name <vanunu24@gmail.com>
2019-03-27 17:15:05 +0200

    sgsert

---
+++ frontend/src/services/cloudinary-service.js
@@ -0,0 +1,37 @@
+export default{
+    doUploadImg
+}
+const CLOUD_NAME = "dmr7h2h2h"
+var UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
+
+// on submit call to this function
+function uploadImg(elForm, ev) {
+    ev.preventDefault();
+   
+    // A function to be called if request succeeds
+    function onSuccess(res) {
+        console.log('uploadedImg', res);
+        console.log('uploadedImg', res.url);
+    }
+
+    doUploadImg(elForm, onSuccess);
+}
+
+function doUploadImg(elForm, onSuccess) {
+    var UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
+    var formData = new FormData();
+    formData.append('file', elForm[0].files[0])
+    formData.append('upload_preset', 'vt0iqgff');
+
+    fetch(UPLOAD_URL, {
+        method: 'POST',
+        body: formData
+    })
+    .then(function (response) {
+        return response.json()
+    })
+    .then(onSuccess)
+    .catch(function (error) {
+        console.error(error)
+    })
+}
