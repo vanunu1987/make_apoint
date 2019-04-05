8a4b51ef3e9fd61bfa347d993cda089e40fa0f71
guyg20056 <guy20056@walla.com>
2019-04-05 21:26:42 +0300

    app nav look change

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,60 +1,65 @@
 <template>
   <div id="app">
-    <button class="nav-burger" @click="isOpenNav = !isOpenNav"><i class="fas fa-bars"></i></button>
+    <button class="nav-burger" @click="isOpenNav = !isOpenNav">
+      <i class="fas fa-bars"></i>
+    </button>
     <userAppoints/>
-          <div id="nav">
-            <div class="logo" v-if="isHome">
-            <h1>Make Appoint<span>.</span></h1>
-            </div>
-            <section class="routes" :class="{ openNav: isOpenNav }">
-            <router-link v-if="loggedUser && loggedUser.business_id"
-            :to="'/business/'+loggedUser.business_id">{{loggedUser.userName}}'s Page <span>|</span></router-link>
-            <router-link @click="test" to="/edit">Add business <span>|</span></router-link>
-            <router-link to="/about">About <span>|</span></router-link>
-            <router-link to="/login">Log in <span>|</span></router-link>
-            <router-link to="/">Home <span>|</span></router-link>
-            </section>
-        </div>
-  <router-view/>
+    <div id="nav">
+      <div class="logo" v-if="isHome">
+        <h1>
+          Make Appoint
+          <span>.</span>
+        </h1>
+      </div>
+      <section class="routes" :class="{ openNav: isOpenNav }">
+        <router-link
+          v-if="loggedUser && loggedUser.business_id"
+          :to="'/business/'+loggedUser.business_id"
+        >{{loggedUser.userName}}'s Page</router-link>
+        <router-link @click="test" to="/edit">Add business</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/login">Log in</router-link>
+        <router-link to="/">Home</router-link>
+      </section>
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import userAppoints from '@/components/UserAppoints.vue'
+import userAppoints from "@/components/UserAppoints.vue";
 export default {
   name: "app",
-  components:{
+  components: {
     userAppoints
   },
   data() {
     return {
-      isOpenNav:false,
-      isHome:true
+      isOpenNav: false,
+      isHome: true
     };
   },
   computed: {
-    loggedUser(){
-      var currUser = this.$store.getters.loggedInUser
-      if (!currUser) return 
-      return currUser
+    loggedUser() {
+      var currUser = this.$store.getters.loggedInUser;
+      if (!currUser) return;
+      return currUser;
     },
-    currBusiness(){
+    currBusiness() {
       console.log(this.$store.getters.currBusiness);
-      return this.$store.getters.currBusiness
+      return this.$store.getters.currBusiness;
     }
   },
   methods: {
-    showReminder(){
-       const h = this.$createElement;
-        this.$notify({
-          title: 'Title',
-          message: h('i', { style: 'color: teal' }, 'This is a reminder')
-    });
+    showReminder() {
+      const h = this.$createElement;
+      this.$notify({
+        title: "Title",
+        message: h("i", { style: "color: teal" }, "This is a reminder")
+      });
     },
-    test(){
-      console.log('ONCLICKKKKKKKKKKKKKKKKKKKKKKKK');
-      this.isHome = false
-      
+    test() {
+      this.isHome = false;
     }
   },
   created() {}
@@ -62,5 +67,30 @@ export default {
 </script>
 
 <style lang="scss">
+#nav {
+  position: absolute;
+  left: 0;
+  z-index: 1000;
+  width: 100%;
 
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 80px;
+  padding-right: 10px;
+  padding-left: 30px;
+  background-image: linear-gradient(to right, white, #ffffff00, white);
+
+  a,
+  .user-name {
+    margin: 0 16px;
+    font-weight: bold;
+    color: #2c3e50;
+    text-decoration: none;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
