0fd4903194f2ef14e4e4af8b832df40cea3952e1
guyg20056 <guy20056@walla.com>
2019-04-06 20:02:09 +0300

    MakeAppointTimeDatePicker makeover

--- frontend/src/components/Details/MakeAppointTimeDatePicker.vue
+++ frontend/src/components/Details/MakeAppointTimeDatePicker.vue
@@ -2,14 +2,18 @@
 
 <template>
   <section class="make-appoint-time-date-picker">
-    <calendar-date-picker 
+    <calendar-date-picker
       class="calendar"
       @to-cmp-appoint-picker="sendToCmpAppointPicker"
       @update-appoint="updateAppoint"
     ></calendar-date-picker>
-    <calendar-appoint-picker  class="appoint-picker" @move-to-cmp="moveToCmp" @update-appoint="updateAppoint" :date="selectedDay">
-    </calendar-appoint-picker>
 
+    <calendar-appoint-picker
+      class="appoint-picker"
+      @move-to-cmp="moveToCmp"
+      @update-appoint="updateAppoint"
+      :date="selectedDay"
+    ></calendar-appoint-picker>
   </section>
 </template>
 
@@ -20,23 +24,18 @@ import CalendarAppointPicker from "@/components/Details/CalendarAppointPicker.vu
 export default {
   data() {
     return {
-      selectedDay: '',
-      showModal:true,
+      selectedDay: Date.now(),
+      showModal: true
     };
   },
   methods: {
     sendToCmpAppointPicker(payload) {
-      console.log("sendToCmpAppointPicker", payload);
       this.selectedDay = payload.value;
     },
     moveToCmp(cmpName) {
       this.$emit("set-cmp", cmpName);
     },
     updateAppoint(payload) {
-      console.log(
-        "update appoint recevied in makeappoint2 cmp with this payload",
-        payload
-      );
       this.$emit("update-appoint", payload);
     }
   },
@@ -48,25 +47,18 @@ export default {
 </script>
 
 <style lang="scss">
-section.make-appoint-time-date-picker{
-  display:flex;
-  position:relative;
-  height: 400px;
- 
-  .appoint-picker{
-    // border: 2px solid grey;
-    // padding: 10px;
+section.make-appoint-time-date-picker {
+  display: flex;
+  justify-content: space-around;
+  position: relative;
+  height: 540px;
+  width: 100%;
+
+  .appoint-picker {
     background-color: white;
-    // top: 33%;
-    // left: -26%;
-    // transform: translate(-50%, -50%);
-    // position: absolute;
     z-index: 4;
     width: 40%;
-}
-  
-
-
+  }
 }
 </style>
 
