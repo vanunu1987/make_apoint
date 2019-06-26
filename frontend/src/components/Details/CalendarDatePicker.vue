3a0c5f3d1c364a9f729796ee636d2dae74740ea4
Your Name <vanunu24@gmail.com>
2019-06-26 15:58:09 +0300

    mhvjhv

--- frontend/src/components/Details/CalendarDatePicker.vue
+++ frontend/src/components/Details/CalendarDatePicker.vue
@@ -180,11 +180,7 @@ div.calendar-button-container {
   .calendar-container{
         max-width: 320px;
      }
-     .calendar-date-picker{
-       display: flex;
-       justify-content: center;
-
-     }
+     
 }
 </style>
 
